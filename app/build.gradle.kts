de379c7d83d5b8e71463dbcfdd9b8820b90f4aaa
Prem Thakur <sdevpremthakur@gmail.com>
2024-02-28 18:12:05 +0530

    setup project

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.example.mymovies"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mymovies"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val nav_version: String by rootProject.extra
+    val lifecycle_version: String by rootProject.extra
+    val room_version: String by rootProject.extra
+    val hilt_version: String by rootProject.extra
+    val coroutines_version: String by rootProject.extra
+    val retrofit_version: String by rootProject.extra
+    val gson_version: String by rootProject.extra
+    val paging_version: String by rootProject.extra
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //paging
+    implementation("androidx.paging:paging-runtime-ktx:$paging_version")
+
+    // navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //view-model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:${hilt_version}")
+    kapt("com.google.dagger:hilt-android-compiler:${hilt_version}")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    //gson
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+}
+kapt {
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val hilt_version by extra { "2.49" }
+    val nav_version by extra { "2.7.6" }
+    val lifecycle_version by extra { "2.7.0" }
+    val coroutines_version by extra { "1.7.3" }
+    val retrofit_version by extra { "2.9.0" }
+    val paging_version by extra { "3.2.1" }
+}
+plugins {
+    val hilt_version: String by extra
+    val nav_version: String by extra
+
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version hilt_version apply false
+    id("androidx.navigation.safeargs.kotlin") version nav_version apply false
+}
